dbee188e65ba5f92cd28cee81600aad9c8ae20e8
Administrator <Administrator@DESKTOP-ISVFPRU>
2021-01-12 17:52:40 +0900

    first

---
+++ board1/WebContent/WEB-INF/jsp/list.jsp
@@ -0,0 +1,37 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>리스트</title>
+</head>
+<body>
+	<div>
+		<a href="/write"><button>글쓰기</button></a>
+	</div>
+	<div>
+		<table>
+			<tr>
+				<th>번호</th>
+				<th>제목</th>
+			</tr>
+			<tr>
+				<th>1</th>
+				<th><a href="/detail?i_board=1">첫번째 글</a></th>
+			</tr>
+			<tr>
+				<th>2</th>
+				<th><a href="/detail?i_board=2">두번째 글</a></th>
+			</tr>
+		</table>
+	</div>
+</body>
+</html>
+
+
+
+
+
+
+
---
+++ board1/src/com/koreait/board/BoardWriteServlet.java
@@ -0,0 +1,29 @@
+package com.koreait.board;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/write")
+public class BoardWriteServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String jsp = "/WEB-INF/jsp/write.jsp";
+		request.getRequestDispatcher(jsp).forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String title = request.getParameter("title");
+		String ctnt = request.getParameter("ctnt");
+		
+		System.out.println("title : " + title);
+		System.out.println("ctnt : " + ctnt);
+		
+		response.sendRedirect("/list");
+	}
+
+}
